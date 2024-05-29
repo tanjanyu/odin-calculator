023ff5035c62a0ffdd3ac40fd2d1210e92c3ebfc
Jan <janyutan@gmail.com>
2024-05-29 17:58:00 +0800

    Add functions for numbers

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// Event handler function
+function eventHandler(event) {
+    let currentTargetID = event.currentTarget.getAttribute("id");
+    switch(true) {
+        case /^n[0-9]+$/.test(currentTargetID):
+            // Function for the number
+            break;
+        
+        case (currentTargetID === ""):
+    }
+}
+
+function addNumber(id) {
+    let screen = document.querySelector(".screen");
+    screen.textContent = screen.textContent + id.charAt(1);
+}
